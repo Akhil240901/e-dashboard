163c8960d4f453e40c01730d5059a9dc2cce2c09
Akhil Bisen <bisenakhil2001@gmail.com>
2024-04-08 23:06:10 +0530

    made post api and added collection to db

--- back-end/index.js
+++ back-end/index.js
@@ -1,1 +1,15 @@
-cons
+const express=require('express');
+const user = require('./db/user');
+require('./db/config');
+
+const app=express();
+
+app.use(express.json());
+
+app.post("/register",async(req,resp)=>{
+    const data=new user(req.body);
+    const result=await data.save();
+    console.log(result)
+    resp.send(result);
+})
+app.listen(5000);
